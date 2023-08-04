12c369b50d40c6dfe0cb813efe1058baf6358386
James Sinkala <xinnks@gmail.com>
2023-08-04 23:28:26 +0300

    docs(tutorials): Add a new codelab on the creation of an e-commerce website using Remix, Drizzle, Turso, and deploying it to Cloudflare Workers

--- tutorials/index.md
+++ tutorials/index.md
@@ -32,6 +32,11 @@ deployment.
 This guide illustrates how to use Fermyon Spin variables to provide values
 required by the [libSQL client library] for Rust.
 
+## [Creating an e-commerce store with Remix, Turso, Drizzle, and Cloudflare Workers]
+
+This tutorial guides you towards the creation of an e-commerce store using
+[Remix], Turso, [Drizzle], and deploying it to [Cloudflare Workers].
+
 ## [Using Turso with Cloudflare Workers and TypeScript]
 
 This tutorial guides you through the process of creating a Cloudflare Worker
@@ -47,6 +52,13 @@ with TypeScript and using that to query a Turso database.
 [Netlify setup guide]: netlify-setup-guide
 [Vercel setup guide]: vercel-setup-guide
 [Fermyon Spin with Rust setup guide]: fermyon-spin-rust-setup-guide
+[Creating an e-commerce store with Remix, Turso, Drizzle, and Cloudflare
+    Workers]: e-commerce-store-codelab
+[Remix]: https://remix.run
+[Drizzle]: https://orm.drizzle.team
+[Cloudflare Workers]: https://workers.cloudflare.com
+
+
 [Using Turso with Cloudflare Workers and TypeScript]: https://developers.cloudflare.com/workers/tutorials/connect-to-turso-using-workers/
 
 [libSQL client library]: /reference/client-access
--- tutorials/sidebars.js
+++ tutorials/sidebars.js
@@ -23,7 +23,7 @@ const sidebars = {
         'get-started-turso-cli/step-07-destroy-logical-database',
         'get-started-turso-cli/step-08-log-out',
         'get-started-turso-cli/turso-cli-review',
-      ]
+      ],
     },
     {
       type: 'doc',
@@ -38,13 +38,31 @@ const sidebars = {
       id: 'fermyon-spin-rust-setup-guide/index',
     },
     {
+      type: 'category',
+      label: 'E-commerce store with Remix, Drizzle & Turso',
+      link: {
+        type: 'doc',
+        id: 'e-commerce-store-codelab/index',
+      },
+      items: [
+        'e-commerce-store-codelab/step-01-setting-up-the-project',
+        'e-commerce-store-codelab/step-02-setting-up-turso',
+        'e-commerce-store-codelab/step-03-configuring-drizzle',
+        'e-commerce-store-codelab/step-04-listing-products',
+        'e-commerce-store-codelab/step-05-user-authentication',
+        'e-commerce-store-codelab/step-06-shopping-cart',
+        'e-commerce-store-codelab/step-07-checking-out',
+        'e-commerce-store-codelab/step-08-deploying-to-cloudflare',
+      ],
+    },
+    {
       type: 'link',
-      label: "Using CloudFlare Workers and TypeScript",
+      label: 'Using CloudFlare Workers and TypeScript',
       href: 'https://developers.cloudflare.com/workers/tutorials/connect-to-turso-using-workers/',
     },
     {
       type: 'link',
-      label: "Example apps and more",
+      label: 'Example apps and more',
       href: 'https://github.com/turso-extended/',
     },
   ],
